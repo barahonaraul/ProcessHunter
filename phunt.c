cf8c001650045f7f5c117b2bd2b2150dee410330
Raul <barahonaraul@live.com>
2016-12-02 14:50:47 -0500

    Test function for printing the date

--- phunt.c
+++ phunt.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+void printDate(){
+	time_t t = time(NULL);
+struct tm tm = *localtime(&t);
+
+printf("now: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+}
+
 int main(){
 
 
